3faed9272a9776c78f80eb7a9721d8744f829ca0
Ardyezapratama <ardyezapratama@gmail.com>
2025-09-12 13:17:56 +0800

    Add relation one to many between User and contact

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Contact extends Model
 {
@@ -12,5 +13,9 @@ class Contact extends Model
     public $incrementing = true;
     public $timestamps = true;
 
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, "user_id", "id");
+    }
 
 }
