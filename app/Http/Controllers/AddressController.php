f62cd91bc79211ecf12d03d2cfddbc41d86cbdcc
Ardyezapratama <ardyezapratama@gmail.com>
2025-09-16 13:25:43 +0800

    Refactoring code (getContact and getAddress)

--- app/Http/Controllers/AddressController.php
+++ app/Http/Controllers/AddressController.php
@@ -7,6 +7,7 @@ use App\Http\Resources\AddressResource;
 use App\Http\Resources\ContactResource;
 use App\Models\Address;
 use App\Models\Contact;
+use App\Models\User;
 use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
@@ -14,9 +15,8 @@ use Illuminate\Support\Facades\Auth;
 
 class AddressController extends Controller
 {
-    public function create(int $idContact, AddressCreateRequest $request): JsonResponse
+    private function getContact(User $user, int $idContact): Contact
     {
-        $user = Auth::user();
         $contact = Contact::where('user_id', $user->id)->where('id', $idContact)->first();
         if (!$contact) {
             throw new HttpResponseException(response()->json([
@@ -27,7 +27,28 @@ class AddressController extends Controller
                 ]
             ])->setStatusCode(404));
         }
+        return $contact;
+    }
 
+    private function getAddress(Contact $contact, int $idAddress): Address
+    {
+        $address = Address::where('contact_id', $contact->id)->where('id', $idAddress)->first();
+        if (!$address) {
+            throw new HttpResponseException(response()->json([
+                'errors' => [
+                    'message' => [
+                        'not found'
+                    ]
+                ]
+            ])->setStatusCode(404));
+        }
+        return $address;
+    }
+    public function create(int $idContact, AddressCreateRequest $request): JsonResponse
+    {
+        $user = Auth::user();
+
+        $contact = $this->getContact($user, $idContact);
         $data = $request->validated();
         $address = new Address($data);
         $address->contact_id = $contact->id;
@@ -39,27 +60,9 @@ class AddressController extends Controller
     public function get(int $idContact, int $idAddress): AddressResource
     {
         $user = Auth::user();
-        $contact = Contact::where('user_id', $user->id)->where('id', $idContact)->first();
-        if (!$contact) {
-            throw new HttpResponseException(response()->json([
-                'errors' => [
-                    'message' => [
-                        'not found'
-                    ]
-                ]
-            ])->setStatusCode(404));
-        }
+        $contact = $this->getContact($user, $idContact);
 
-        $address = Address::where('contact_id', $contact->id)->where('id', $idAddress)->first();
-        if (!$address) {
-            throw new HttpResponseException(response()->json([
-                'errors' => [
-                    'message' => [
-                        'not found'
-                    ]
-                ]
-            ])->setStatusCode(404));
-        }
+        $address = $this->getAddress($contact, $idAddress);
 
         return new AddressResource($address);
     }
